63e748bea1232980f1d5a08f4b6e17fa4e82678b
Meet8080 <patelarjun687@gmail.com>
2022-06-30 11:44:41 -0400

    portfolio

---
+++ script.js
@@ -0,0 +1,24 @@
+import Typed from "typed.js";
+// function autotext() {
+//   const text = document.getElementById("effect");
+//   let index = 0;
+
+//   //   console.log(text);
+//   text.innerHTML = text.toString().slice(0, index);
+
+//   //   console.log(typeof ss);
+//   //   console.log(text.innerHTML);
+//   index++;
+
+//   if (index >= text.length) {
+//     index = 0;
+//   }
+// }
+// setInterval(autotext, 1000);
+
+var Typed = new Typed(".extras", {
+  strings: ["Front-end", "Web"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
